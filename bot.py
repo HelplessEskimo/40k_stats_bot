43e7c8323d47c0fadf98eb987eed123d89fa16dd
HelplessEskimo <helpless.eskimo@fuckyou.net>
2021-06-18 22:55:16 +0100

    intiail commit

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+
+client = discord.Client()
+
+unit_dict = {
+    "Primaris Lieutenant": {
+        "Ws": "3+",
+        "Bs": "3+",
+        "Sv": "3+"
+    }
+}
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$unit'):
+        parsed = parse_msg(message.content[6:])
+        if not parsed:
+            await message.channel.send("lookup failed")
+        await message.channel.send(str(unit_dict_lookup(parsed[0])))
+
+
+def parse_msg(msg):
+    if msg == "":
+        return False
+
+    units = msg.split(",")
+    return units
+
+
+def unit_dict_lookup(unit):
+    found_unit = unit_dict[unit]
+    return found_unit
+
+
+client.run('ODU1NDUzNjczNDA4MTY3OTQ2.YMytUQ.MVTN6U5hkaPSwWe11VN96JRIJRY')
